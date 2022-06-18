b066076cb7cc114d2966617662d48a27b47fbe35
peterndta <truonganh20062001@gmail.com>
2022-06-18 14:04:46 +0700

    18062022 - Add movement with event listener (DONE)

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height); // create new rectangle đại diện cho nhân vật
 
-const gravity = 0.2
+const gravity = 0.6
 
 class Sprite{
     constructor({ position, velocity }) {
         this.position = position
         this.velocity = velocity
         this.height = 150
+        
+        // lastKey dùng để keep track phím cuối được nhấn cho vào dòng if below
+        // để tránh việc khi press A thì k press D đc vì if vào A đầu tiên 
+        this.lastKey
     }
 
     draw(){
@@ -54,14 +58,13 @@ const keys = { // để khắc phục việc buôn 1 phím nhưng dừng movemen
     d: {
         pressed: false
     },
-    w: {
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
         pressed: false
     }
 }
-// dùng để keep track phím cuối được nhấn cho vào dòng if below
-// để tránh việc khi press A thì k press D đc vì if vào A đầu tiên 
-let lastKey 
-
 
 function animate(){
     window.requestAnimationFrame(animate) // loop funct animate lại nhiều lần
@@ -71,13 +74,23 @@ function animate(){
     player.update()
     enemy.update()
     
-    player.velocity.x = 0 // dừng model character khi bỏ tay ra
+    player.velocity.x = 0 // dừng model player character khi bỏ tay ra
+    enemy.velocity.x = 0 // dừng model enemy character khi bỏ tay ra
 
-    if(keys.a.pressed && lastKey === 'a'){ // nếu pressed = true
-        player.velocity.x = -1
+    // player movement 
+    if(keys.a.pressed && player.lastKey === 'a'){ // nếu pressed = true
+        player.velocity.x = -5
     } 
-    else if (keys.d.pressed && lastKey === 'd'){ 
-        player.velocity.x = 1
+    else if (keys.d.pressed && player.lastKey === 'd'){ 
+        player.velocity.x = 5
+    }
+
+    // enemy movement 
+    if(keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft'){ // nếu pressed = true
+        enemy.velocity.x = -5
+    } 
+    else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){ 
+        enemy.velocity.x = 5
     }
 }
 animate()
@@ -86,21 +99,36 @@ window.addEventListener( 'keydown', (event) => {
     switch ( event.key){
         case 'd':
             keys.d.pressed = true; // di chuyển 1 px mỗi khi ấn d
-            lastKey = 'd'
+            player.lastKey = 'd'
             break
         case 'a':
             keys.a.pressed = true;
             // di chuyển 1 px mỗi khi ấn a
-            lastKey = 'a'
+            player.lastKey = 'a'
             break
         case 'w':
             // bay lên - hạ xuống được nhờ có gravity, vì gravity luôn kéo model xuống nếu như lơ lững (k gần edge rìa)
-            player.velocity.y = -10 // thêm 10px vào trục y
+            player.velocity.y = -10 // thêm 20px vào trục y để nhảy
+            break
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true; // di chuyển 1 px mỗi khi ấn mũi tên phải
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true;
+            // di chuyển 1 px mỗi khi ấn mũi tên trái
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            // bay lên - hạ xuống được nhờ có gravity, vì gravity luôn kéo model xuống nếu như lơ lững (k gần edge rìa)
+            enemy.velocity.y = -10 // thêm 20px vào trục y để nhảy
             break
     }
 })
 
 window.addEventListener( 'keyup', (event) => {
+    // player
     switch ( event.key){
         case 'd':
             keys.d.pressed = false; // di chuyển 1 px mỗi khi ấn d
@@ -108,8 +136,15 @@ window.addEventListener( 'keyup', (event) => {
         case 'a':
             keys.a.pressed = false; // di chuyển 1 px mỗi khi ấn a
             break
-        case 'w':
-            keys.w.pressed = false; // di chuyển 1 px mỗi khi ấn w
+    }
+
+    // enemy
+    switch ( event.key){
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false; // di chuyển 1 px mỗi khi ấn d
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false; // di chuyển 1 px mỗi khi ấn a
             break
     }
 })
